81966376f0d2f7aea5f4f392f9f00b6092fc2abd
ClumsyJackdaw <clumsyjackdaw@gmail.com>
2025-09-02 22:20:49 +0200

    Finish select age screen

--- scripts/game.js
+++ scripts/game.js
@@ -1,5 +1,9 @@
+let selectedAge = 0
+
 function iAmOver18Clicked() {
     document.getElementById("iAmOver18Screen").style.display = "none";
+    document.getElementById("title-txt-img").style.display = "block";
+    document.getElementById("selectAgeScreen").style.display = "block";
 }
 
 function selectAgeSetup() {
@@ -10,14 +14,29 @@ function selectAgeSetup() {
         ageOrder.push(i);
     }
 
-    const ageContainer = document.getElementById("ageContainer");
+    const ageContainer = document.getElementById("agesInHere");
     ageContainer.style.display = "grid";
     ageContainer.style.gridTemplateColumns = "repeat(10, 1fr)"
-    ageContainer.style.gap = "8px 16px";
+
+    for (let i = ageOrder.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [ageOrder[i], ageOrder[j]] = [ageOrder[j], ageOrder[i]];
+    }
 
     ageOrder.forEach((age) => {
         const label = document.createElement("label");
-        label.innerHTML = `<input type="radio" name="choice" value="${age}"> ${age}`
+        label.innerHTML = `<input type="radio" name="selectedAge" value="${age}"> ${age}`
+        label.style.backgroundColor = "#a1bfc7"
         ageContainer.appendChild(label);
     })
+}
+
+function selectAge() {
+    if (!document.querySelector('input[name="selectedAge"]:checked')) {
+        alert("Please select an age.");
+        event.preventDefault();
+    } else {
+        selectedAge = parseInt(document.querySelector('input[name="selectedAge"]:checked').value);
+        document.getElementById("selectAgeScreen").style.display = "none";
+    }
 }
