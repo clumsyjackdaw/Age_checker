6e4d7cf0dc7e8c4f5c2d7f6ea6b565187ac3a3c8
ClumsyJackdaw <clumsyjackdaw@gmail.com>
2025-09-02 21:04:16 +0200

    This is not working, let's just shove everything in index again

--- scripts/game.js
+++ scripts/game.js
@@ -1,3 +1,23 @@
 function iAmOver18Clicked() {
     document.getElementById("iAmOver18Screen").style.display = "none";
 }
+
+function selectAgeSetup() {
+    const maxAge = 99
+    let ageOrder = []
+
+    for (let i = 0; i <= maxAge; i++) {
+        ageOrder.push(i);
+    }
+
+    const ageContainer = document.getElementById("ageContainer");
+    ageContainer.style.display = "grid";
+    ageContainer.style.gridTemplateColumns = "repeat(10, 1fr)"
+    ageContainer.style.gap = "8px 16px";
+
+    ageOrder.forEach((age) => {
+        const label = document.createElement("label");
+        label.innerHTML = `<input type="radio" name="choice" value="${age}"> ${age}`
+        ageContainer.appendChild(label);
+    })
+}
